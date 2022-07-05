5aeca303ed5c5d0fdb5d53037c8e7f89288c7421
codinci <dkibau@gmail.com>
2022-07-05 17:25:59 +0300

    Refractoring

--- app.js
+++ app.js
@@ -5,12 +5,32 @@ let brushSelector = null;
 let bgColor = '#fbf8f3';
 
 function setup() {
+    setupToolBox();
+    //set up the canvas
+    canvas = createCanvas(windowWidth - toolboxWidth, windowHeight);
+    canvas.parent(select('section.canvas'));
+    background(bgColor);
+}
+
+function draw() {
+    //check if mouse button is pressed and mouse is hovering over the canvas
+    if(mouseIsPressed && mouseX <= windowWidth - toolboxWidth) {
+        //draw on the canvas with the selected painting tool function
+        window[selectedTool]();
+    }
+}
+
+function setupToolBox() {
+    //select the paint style tools in the Styles category
+    const paintStyles = select('section.toolbox div.styles-tools');
+    setupBrushSelector(paintStyles);
+}
+
+function setupBrushSelector(parentTag) {
     //create the brush selector as a <select> tag
     brushSelector = createSelect();
-    //select the styles category in the toolbox
-    const paintStyles = select('section.toolbox div.styles-tools');
     // put the brush selector dropdown menu inside of the Styles container
-    brushSelector.parent(paintStyles);
+    brushSelector.parent(parentTag);
 
     //make an array of all paintbrush names
     const brushes = [
@@ -37,17 +57,5 @@ function setup() {
     brushSelector.changed(function () {
         selectedTool = brushSelector.value();
     });
-
-    //set up the canvas
-    canvas = createCanvas(windowWidth - toolboxWidth, windowHeight);
-    canvas.parent(select('section.canvas'));
-    background(bgColor);
 }
 
-function draw() {
-    //check if mouse button is pressed and mouse is hovering over the canvas
-    if(mouseIsPressed && mouseX <= windowWidth - toolboxWidth) {
-        //draw on the canvas with the selected painting tool function
-        window[selectedTool]();
-    }
-}
